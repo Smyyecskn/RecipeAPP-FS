5a1eef055728e8a22c0792513cc4e6ab31130231
smyye <smyyeoztrk43@gmail.com>
2024-09-30 13:25:39 +0300

    not found

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <img
+        src="https://cdn.prod.website-files.com/5db19236083f4b42bea1dbe5/62e25aaf4bbedf5fef74cddb_404-not-found.png"
+        alt="not-found"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
