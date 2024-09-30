ac769bcf5c82485d57b8e2e681790e930a3647d2
smyye <smyyeoztrk43@gmail.com>
2024-09-30 15:49:25 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import meal2 from "../assets/meal2.svg";
 
 const Header = () => {
   return (
-    <div className="mx-auto head bg-light border border-dark">
+    <div className="mx-auto head  border border-dark">
       <img width="50px" src={meal2} alt="meal" />
       <h2>🍽️SMYYE RECIPE APP </h2>
     </div>
